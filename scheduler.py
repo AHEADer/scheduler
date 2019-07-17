1600933c4cae625f131e86490063c2593b4ee1aa
AHEADer <dqawveird@gmail.com>
2019-07-17 15:46:22 +0800

    mostly done, need to test structure

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import threading
 import queue
 import time
+import heapq
 from estimator import Estimator
 # First try for 5 epochs and then analysis
 # Predict the converged epoch, use patience strategy
@@ -14,13 +15,14 @@ from estimator import Estimator
 
 class Scheduler:
     def __init__(self, node_list, gpu_per_node, job_executor):
-        self.job_queue = queue.Queue()
+        self.job_queue = []
         self.init_job_queue = queue.Queue()
         self.cluster_resource = self.cluster_init(node_list, gpu_per_node)
         self.gpu_per_node = gpu_per_node
         self.current_wait_job = None
         self.job_executor = job_executor
         self.unpredicted_job = queue.Queue()
+        self.estimator = Estimator()
         self.msg_handler = threading.Thread(target=self._msg_handle, args=())
         self.msg_handler.start()
 
@@ -36,14 +38,13 @@ class Scheduler:
             else:
                 if self.init_job_queue.empty():
                     # no new job's coming, check jobs that finish initial step
-                    if self.job_queue.empty():
-                        # no jobs done nothing
-                        pass
-                    else:
-                        job_info = self.job_queue.get()
+                    if len(self.job_queue) > 0:
+                        # first update the priority queue
+                        self.update_job_queue()
+                        print('train job exec')
+                        job_info = heapq.heappop(self.job_queue)[1]
                         self.occupy_gpu(gpu_info)
                         self.execute_job(job_info, gpu_info)
-                        pass
                 else:
                     job_info = self.init_job_queue.get()
                     print('new job coming!')
@@ -53,6 +54,14 @@ class Scheduler:
                     # No new job coming, sleep some time
 
             # There's priority: trial job > less time job/long waited job > long time job
+
+    def update_job_queue(self):
+        current_tm = time.time()
+        for i in range(len(self.job_queue)):
+            self.job_queue[i][1]['wait_tm'] += current_tm - self.job_queue[i][1]['cpt_tm']
+            self.job_queue[i][0] = self.priority(self.job_queue[i][1]['wait_tm'], self.job_queue[i][1]['r_tm'])
+
+        heapq.heapify(self.job_queue)
 
     def execute_job(self, job_info, gpu_info):
         # job info from generator or init step
@@ -86,10 +95,10 @@ class Scheduler:
     def release_gpu(self, gpu_info):
         self.cluster_resource[gpu_info[0]][int(gpu_info[1])] = 1
 
-    def priority(self, job_info):
-        # basic:
-
-        pass
+    @staticmethod
+    def priority(wait_tm, r_tm):
+        # basic: r_tm must be more important than wait_tm
+        return r_tm/(wait_tm+5*60)
 
     def init_enqueue(self, job_info):
         self.init_job_queue.put(job_info)
@@ -100,8 +109,7 @@ class Scheduler:
         # get predict time
         # TODO change 5 to a parameter for scheduler
         tm_per_epoch = job_info['tm_per_ep']
-        est = Estimator()
-        epoch = est.resnet_predict(job_info['init_f'], 0.01)
+        epoch = self.estimator.resnet_predict(job_info['init_f'], 0.01)
         if epoch == -1:
             # abnormal training, unpredictable！
             self.unpredicted_job.put(job_info)
@@ -110,5 +118,7 @@ class Scheduler:
             job_info['r_tm'] = remaining_tm
             job_info['priority'] = 0
             job_info['join2_tm'] = time.time()
-            self.job_queue.put(job_info)
+            job_info['wait_tm'] = job_info['join2_tm'] - job_info['cpt_time'] + job_info['wait_tm']
+            priority = self.priority(job_info['wait_tm'], remaining_tm)
+            heapq.heappush(self.job_queue, (priority, job_info))
         pass
