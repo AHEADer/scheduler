6fc8078051c86b6f9b4ffce0adde0288b6966de7
Zhang Junda <dqawveird@gmail.com>
2019-09-11 19:57:36 +0800

    add msg handler to VGG training

--- VGG-cifar/examples/vgg_cifar.py
+++ VGG-cifar/examples/vgg_cifar.py
@@ -9,6 +9,9 @@ import platform
 import argparse
 import numpy as np
 import tensorflow as tf
+import threading
+import socket
+import json
 
 sys.path.append('../')
 import loader as loader
@@ -19,10 +22,20 @@ from src.helper.evaluator import Evaluator
 
 # DATA_PATH = '/home/junda/cifar-10-batches-py'
 # SAVE_PATH = '/home/junda/vgg19/'
+scheduler_op = None
+target_gpus = []
+
+def binary_to_dict(the_binary):
+    jsn = the_binary.decode('utf-8')
+    d = json.loads(jsn)
+    return d
 
 
 def get_args():
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('--port', type=int,
+                        help='Listen port for scheduling message')
 
     parser.add_argument('--data_path', type=str,
                         help='Specify data location')
@@ -75,6 +88,13 @@ def train():
         for epoch_id in range(FLAGS.maxepoch):
             trainer.train_epoch(sess, keep_prob=FLAGS.keep_prob, summary_writer=writer)
             trainer.valid_epoch(sess, dataflow=valid_data, summary_writer=writer)
+            if scheduler_op == 'g' or 's':
+                saved_model = '{}vgg-cifar-epoch-{}'.format(FLAGS.save_path, epoch_id)
+                saver.save(sess, saved_model)
+                # generate command
+                config_command = '--train --port ' + str(FLAGS.port) + ' --data_path '
+                # send command back to the scheduler
+                pass
             # saver.save(sess, '{}vgg-cifar-epoch-{}'.format(SAVE_PATH, epoch_id))
         saver.save(sess, '{}vgg-cifar-epoch-{}'.format(FLAGS.save_path, epoch_id))
 
@@ -82,7 +102,7 @@ def train():
 def evaluate():
     FLAGS = get_args()
     train_data, valid_data = loader.load_cifar(
-        cifar_path=DATA_PATH, batch_size=FLAGS.bsize, substract_mean=True)
+        cifar_path=FLAGS.data_path, batch_size=FLAGS.bsize, substract_mean=True)
 
     valid_model = VGG_CIFAR10(
         n_channel=3, n_class=10, bn=True, sub_vgg_mean=False)
@@ -93,15 +113,36 @@ def evaluate():
     with tf.Session() as sess:
         saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
-        saver.restore(sess, '{}vgg-cifar-epoch-{}'.format(SAVE_PATH, FLAGS.load))
+        saver.restore(sess, '{}vgg-cifar-epoch-{}'.format(FLAGS.save_path, FLAGS.load))
         print('training set:', end='')
         evaluator.accuracy(sess, train_data)
         print('testing set:', end='')
         evaluator.accuracy(sess, valid_data)
 
+
+def msg_handle(port):
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    connection.bind(('0.0.0.0', port))
+    connection.listen(10)
+    while True:
+        current_connection, address = connection.accept()
+        data = current_connection.recv(2048)
+        msg = binary_to_dict(data)
+        if msg['op'] == 'sg' or 'ss':
+            # stop previous grow/shrink command
+            scheduler_op = 'stop'
+        elif msg['op'] == 'g':
+            scheduler_op = 'g'
+        elif msg['op'] == 's':
+            scheduler_op = 's'
+        target_gpus = msg['gpus']
+
+
 if __name__ == "__main__":
     FLAGS = get_args()
-
+    # build message pipeline
+    msg_handler = threading.Thread(target=msg_handle, args=(FLAGS.port,))
     if FLAGS.train:
         train()
     if FLAGS.eval:
